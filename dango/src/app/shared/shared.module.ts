c338a37fa82962588389320397f6f931febce0e4
karlkaspar <karlkasparkulesha@gmail.com>
2020-10-30 14:28:27 +0200

    DANGO-1: Added services, shared module, footer & header, common module.

---
+++ dango/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoadingSpinnerComponent } from './_common/loading-spinner/loading-spinner.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, LoadingSpinnerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
